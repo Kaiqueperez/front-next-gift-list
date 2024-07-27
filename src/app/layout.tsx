084c551bccf84f177c7b79c327418bb998be1e22
Kaique Perez <kaiqueperezz@gmail.com>
2024-07-27 18:22:35 -0300

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { ModalProvider, WishListProvider } from '@/contexts'
 import ThemeRegistry from '@/theme/ThemeRegistry'
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -12,6 +14,16 @@ export default function RootLayout({
   return (
     <ModalProvider>
       <html lang="pt-br">
+        <Script strategy='afterInteractive' async src="https://www.googletagmanager.com/gtag/js?id=G-1J3433RX9G" />
+        <Script id='gtm-script'>
+          {
+            ` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-1J3433RX9G');`
+          }
+        </Script>
         <WishListProvider>
           <ThemeRegistry>
             <body>
